898ca62cc9b6bafd02af4bd0fbbe6d96cb6335ff
Ahmad Nur Aziz <ahmadnuraziz3@gmail.com>
2019-07-12 07:07:46 +0700

    Update connect.m

--- connect.m
+++ connect.m
@@ -1,14 +1,29 @@
 function edge = connect(data,N)
-%this function connect nodes only for undirected case
     pair = [];
     edge = [];
-    for i=1:N
-        for j=i:N
-            if i~=j
-                if ~isnan(data(i,j))
-                    if data(i,j) == 1
-                        pair = [i,j];
-                        edge = [edge ;pair];
+    type = 'undirected';
+    if type == 'undirected'
+        for i=1:N
+            for j=i:N
+                if i~=j
+                    if ~isnan(data(i,j))
+                        if data(i,j) == 1
+                            pair = [i,j];
+                            edge = [edge ;pair];
+                        end
+                    end
+                end
+            end
+        end
+    else
+        for i=1:N
+            for j=1:N
+                if i~=j
+                    if ~isnan(data(i,j))
+                        if data(i,j) == 1
+                            pair = [i,j];
+                            edge = [edge ;pair];
+                        end
                     end
                 end
             end
